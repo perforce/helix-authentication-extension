a30d05728197224e754587ad65f0c5211631fd72
Nathan Fiedler <nfiedler@perforce.com>
2020-04-30 14:54:40 -0700

    Ensure the Swarm log is writable by Apache user

--- containers/swarm/run.sh
+++ containers/swarm/run.sh
@@ -6,8 +6,9 @@ set -e
 if [ -x /opt/perforce/swarm/sbin/redis-server-swarm ]; then
     /opt/perforce/swarm/sbin/redis-server-swarm /opt/perforce/etc/redis-server.conf --daemonize yes
 fi
+touch /opt/perforce/swarm/data/log
+chown www-data:www-data /opt/perforce/swarm/data/log
 apachectl start
 echo Rebar123 | p4 -p p4d.doc:1666 -u swarm login -p > ticket
 sed -e "s/REPLACEME/$(awk 'NR > 1' ticket)/" /opt/perforce/swarm/data/config.php.template > /opt/perforce/swarm/data/config.php
-touch /opt/perforce/swarm/data/log
 exec tail -f /opt/perforce/swarm/data/log
