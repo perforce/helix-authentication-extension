7b168a88cd72094f89f4f924590f90276cb2672e
Nathan Fiedler <nfiedler@perforce.com>
2020-07-17 11:45:44 -0700

    chore: refine docker build process for p4d

--- containers/p4d/build.sh
+++ containers/p4d/build.sh
@@ -13,6 +13,9 @@ P4PASSWD=Rebar123
 p4dctl start -o '-p 0.0.0.0:1666' despot
 echo ${P4PASSWD} | p4 login
 
+# disable the signed extensions requirement for testing
+# p4 configure set server.extensions.allow.unsigned=1
+
 #
 # install and configure the extension
 #
@@ -59,9 +62,6 @@ p4 configure set auth.default.method=ldap
 p4 configure set auth.ldap.order.1=simple
 p4 configure set auth.sso.allow.passwd=1
 p4 configure set auth.sso.nonldap=1
-
-# disable the signed extensions requirement for testing
-# p4 configure set server.extensions.allow.unsigned=1
 
 #
 # stop the server so that the run script can start it again,
