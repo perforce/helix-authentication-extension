da2051d081a59821c459742c810acb6745990bb2
Nathan Fiedler <nfiedler@perforce.com>
2020-10-05 12:40:15 -0700

    Minor fixes to the Swarm docker setup

--- containers/swarm/run.sh
+++ containers/swarm/run.sh
@@ -10,5 +10,8 @@ touch /opt/perforce/swarm/data/log
 chown www-data:www-data /opt/perforce/swarm/data/log
 apachectl start
 echo Rebar123 | p4 -p p4d.doc:1666 -u swarm login -p > ticket
+if [ ! -e /opt/perforce/swarm/data/config.php.original ]; then
+    cp /opt/perforce/swarm/data/config.php /opt/perforce/swarm/data/config.php.original
+fi
 sed -e "s/REPLACEME/$(awk 'NR > 1' ticket)/" /opt/perforce/swarm/data/config.php.template > /opt/perforce/swarm/data/config.php
 exec tail -f /opt/perforce/swarm/data/log
